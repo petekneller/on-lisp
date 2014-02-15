fca9ee20a6ab94126c2ef9be880a7c6893a204f7
petekneller <mrkneller@hotmail.com>
2014-02-15 14:20:06 +0000

    finished chapter 10 recursion problems with macros

--- src/on_lisp/chapter10_other_macro_pitfalls.clj
+++ src/on_lisp/chapter10_other_macro_pitfalls.clj
@@ -1,3 +1,40 @@
-(ns on-lisp.chapter10-other-macro-pitfalls)
+(ns on-lisp.chapter10-other-macro-pitfalls
+  (:require [on-lisp.lisp-do.public :refer :all]))
 
 ; 10.4 Recursion
+
+(defn ntha [n coll]
+  (if (= n 0)
+    (first coll)
+    (ntha (- n 1) (rest coll))))
+
+(defmacro nthb [n coll]
+  `(if (= ~n 0)
+     (first ~coll)
+     (nthb (- ~n 1) (rest ~coll))))
+
+(defmacro nthc [n coll]
+  `(loop [n# ~n
+          coll# ~coll]
+     (if (= n# 0)
+       (first coll#)
+       (recur (- n# 1) (rest coll#)))))
+
+(defn or-expand [args]
+  (if (empty? args)
+    nil
+    `(let [sym# ~(first args)]
+       (if sym#
+         sym#
+         ~(or-expand (rest args))))))
+
+(defmacro ora [& args]
+  (or-expand args))
+
+(defmacro orb [& args]
+  (if (empty? args)
+    nil
+    `(let [sym# ~(first args)]
+       (if sym#
+         sym#
+         (orb ~@(rest args))))))
