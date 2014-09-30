006780471dab44a2dfb93c5b43d7b1cca45f35c4
petekneller <mrkneller@hotmail.com>
2014-09-30 13:10:50 +0100

    adding some examples of my own of pattern matching with ?-symbols

---
+++ src/on_lisp/chapter18_destructuring_matching.clj
@@ -0,0 +1,16 @@
+(ns on-lisp.chapter18-destructuring-matching)
+
+(defn vector-matcher [form arg]
+  (let [pairs (map vector form arg)]
+    (cond
+      :else `(every? true? ~(vec (map #(apply form-matcher %) pairs))))))
+
+(defn form-matcher [form arg]
+  (cond
+    (vector? form) (vector-matcher form arg)
+    (and (symbol? form) (.startsWith (name form) "?")) `true
+    :else `(= ~arg ~form)))
+
+(defmacro match [forms arg]
+  (let [one (first forms)]
+    (form-matcher one (eval arg))))
---
+++ test/on_lisp/chapter18_destructuring_matching_test.clj
@@ -0,0 +1,28 @@
+(ns on-lisp.chapter18-destructuring-matching-test
+  (:require [midje.sweet :refer :all]
+            [on-lisp.chapter18-destructuring-matching :refer :all]))
+
+(fact "match correctly recognises literals and keywords"
+      (match [1] 1) => true
+      (match [false] false) => true
+      (match ["foo"] "foo") => true
+      (match [:foo] :foo) => true
+      (macroexpand `(match [1] 1)) => `(= 1 1))
+
+(fact "match correctly recognises symbol values"
+      (let [i 1
+            bar "bar"]
+        (match [i] 1) => true
+        (match [bar] "bar") => true
+        (macroexpand `(match [bar] "bar")) => `(= "bar" bar)))
+
+(fact "match ignores symbols with name beginning in question mark"
+      (match [?a] "foo") => true
+      (match [?_] "foo") => true
+      (macroexpand `(match [?_] "foo")) => `true)
+
+(fact "match performs recursive destructuring for vectors"
+      (match [[1 ?b 3]] (vector 1 2 3)) => true
+      (match [[[1 ?b] [3]]] [(vector 1 2) (vector 3)]) => true
+      (macroexpand `(match [[1 ?b 3]] (vector 1 2 3))) => `(every? true? [(= 1 1) true (= 3 3)]))
+; boundary conditions in the above?
