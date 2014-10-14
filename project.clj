79fbdf719e55f00099a81572921ed7e5c66860c6
petekneller <mrkneller@hotmail.com>
2014-10-14 18:47:40 +0100

    finally got the matching working, but had to just rewrite it my own way

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
 (defproject on-lisp "1.0-SNAPSHOT"
-            :dependencies [[org.clojure/clojure "1.6.3"]
-                           [midje "1.6.0"]]
-            :plugins [[lein-midje "3.1.1"]])
+  :dependencies [[midje "1.6.0"]]
+  :plugins [[lein-midje "3.1.1"]])
