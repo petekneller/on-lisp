9657e2f1e77941b881a7a081e5956ec2b445d1a8
petekneller <mrkneller@hotmail.com>
2014-10-14 15:59:06 +0100

    Working on the query compiler; currently stuck! figuring out how to bind resolved symbols

--- src/on_lisp/chapter19_query_compiler.clj
+++ src/on_lisp/chapter19_query_compiler.clj
@@ -1,1 +1,23 @@
-(ns on-lisp.chapter19-query-compiler)
+(ns on-lisp.chapter19-query-compiler
+  (:require [on-lisp.chapter18-destructuring-matching :refer :all]
+            [on-lisp.chapter19-query-interpreter :refer [db-query vars-in]]))
+
+(defmacro with-gensyms [vars & body]
+  `(let ~(vec (mapcat (fn [var] [var (gensym)]) vars))
+     ~@body))
+
+(declare compile-query)
+
+(defn compile-simple [query body]
+  `(for [fact# (db-query ~(first query))]
+     (if (match? ~(second query) fact#)
+       ~@body)))
+
+(defn compile-query [query body]
+  (cond
+    :else (compile-simple query body)))
+
+(defmacro with-answer [query & body]
+  `(with-gensyms ~(vars-in query)
+       ~(compile-query query `(do ~@body))))
+
--- test/on_lisp/chapter19_query_compiler_test.clj
+++ test/on_lisp/chapter19_query_compiler_test.clj
@@ -1,3 +1,35 @@
 (ns on-lisp.chapter19-query-compiler-test
   (:require [midje.sweet :as m]
-            [on-lisp.chapter19-query-compiler :refer :all]))
+            [on-lisp.chapter19-query-compiler :refer :all]
+            [on-lisp.chapter19-query-interpreter-test :refer [set-test-data]]))
+
+
+(m/fact "with-answer"
+        (set-test-data)
+        (m/fact "simple"
+              (with-answer [:painter ["hogarth" ?x ?y]]
+                           [?x ?y]) => (m/just [["william" "english"]]))
+        (m/fact "and combinator"
+              (with-answer [:and
+                            [:painter [?x _ _]]
+                            [:dates [?x 1697 _]]]
+                           ?x) => (m/just ["canale" "hogarth"]))
+        (m/fact "or combinator"
+              (with-answer [:or
+                            [:dates [?x ?y 1772]]
+                            [:dates [?x ?y 1792]]]
+                           [?x ?y]) => (m/just [["hogart" 1697] ["reynolds" 1723]] :in-any-order))
+        (m/fact "not combinator"
+              (with-answer [:and
+                            [:painter [?x _ "english"]]
+                            [:dates ?x ?b _]
+                            [:not [:and
+                                   [:painter [?x2 _ "venetian"]]
+                                   [:dates ?x2 ?b _]]]]
+                           ?x) => (m/just "reynolds"))
+        (m/fact "arbitrary clojure"
+              (with-answer [:and
+                            [:painter ?x _ _]
+                            [:dates ?x _ ?d]
+                            [:lisp (< 1770 ?d 1800)]]
+                           [?x ?d]) => (m/just [["reynolds" 1792] ["hogarth" 1772]])))
